7351299017996b9c3184488645d0787367d46b69
Alice Santilli <alice.sant@gmail.com>
2016-10-12 13:06:16 -0500

    permissionSaver-whole_directory

--- permissionSaver.sh
+++ permissionSaver.sh
@@ -3,17 +3,15 @@
 #permission saver - creates a new file every time it runs to save updated permissions
 # check if saved permissions exists and the creates saved permission 1-n
 
-read -p "what's the filename of the permission's you want saved"  filename
+
 
 for ((i=1;i<=100;i++)) ; do 
 	if [ -f savedPermissions$i ] ; then
 		continue;
 	else 
-		savedPermission=$(ls -lha $filename | awk '{print $1}') 
-		echo $savedPermission > savedPermissions$i
+		ls -lha | awk '{print $1 " " $9}' > savedPermissions$i
 		break;
 	fi
 done
-
-	echo savedPermissions has been created 
+	echo savedPermissions$i  has been created 	
 
