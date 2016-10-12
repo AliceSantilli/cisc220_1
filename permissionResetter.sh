94e8968ba7820a6e11a3022749c3b00e359d5abe
dimontejulia <dimontejulia@gmail.com>
2016-10-12 12:13:48 -0500

    initial commit

---
+++ permissionResetter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#permission resetter - if user presses, s: show permissions that were changed, r: reset changed permission back to old one
+
+
+	read var1 var2
+	reset=-r
+	show=-s
+
+	if [[ var1 = show ]] ; do
+	
+	elif [[ var1 == reset ]] ; do
+
+
+	
+
+
+
+
+		
+
