fe44a3a1f3d2dc193179026c5aceedc8918c8e6d
dimontejulia <dimontejulia@gmail.com>
2016-10-12 12:46:32 -0500

    commenting at top and tried to fix catching invalid sign

--- Question1.sh
+++ Question1.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Arithmetic Operation Question
-# need to add looping structure etc
+#Question1 -  Arithmetic Operation Question
+#Julia Di Monte  - 10175838
+#Bronwyn Gemmill - 10170028
+#Alice Santilli  - 10178318
 
 	read -p "Please insert a number: " var1
 
-check=1
-exit=:q
+	check=1
+	exit=:q
 
 	while [[ $check -eq 1 ]] ; do 
 
@@ -15,17 +17,19 @@ exit=:q
 		echo Thank you for using the calculator
 		break
 	fi
-		#PROBLEM - CHECK IF INPUT SIGN IS CORRECT
 
 		read -p "please insert a number: " var2
 
 	if [[ $var2 = $exit ]] ; then
 		echo Thank you for using the calculator
-		break ; 
-	fi
-		
+		break  
+	elif [[ $var2 == + ]] || [[ $var2 == - ]] || [[ $var2 == * ]] || [[ $var2 == / ]] ; then
 		var1=$(($var1$sign$var2))
+		echo Result so far: $var1
 
-		echo Result so far:  $var1
+	else 
+		echo Sorry! Invalid Input ;
+
+	fi 
 
 	done                                
