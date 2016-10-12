ed7dee5f2a310dfbf4171e4f79300dc6ea94318b
Alice Santilli <alice.sant@gmail.com>
2016-10-12 11:24:32 -0500

    Question2.sh-working

--- Question2.sh
+++ Question2.sh
@@ -4,10 +4,12 @@
 
 	IP=$(ifconfig | grep -e "inet addr" | awk '{print $2}'  | awk -F "addr:" '{print $2}')
 	
+	
 	for line in $IP ; do
-		if [[$line==127* ]] ; then
+
+		if [[ $line == 127* ]] ; then
 			echo LocalIP $line ;
-		elif [[$line==192.168* || $line==10* ]] ; then 
+		elif [[ $line == 192.168* || $line == 10* ]] ; then 
 			echo PrivateIP $line ;
 		else
 			echo PublicIP $line ;
