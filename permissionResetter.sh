c13631f212a3773241dad1a8d1fa78241a3927cd
14bvg1 <14bvg1@queensu.ca>
2016-10-12 14:10:43 -0400

    permissionResetter.sh-start

--- permissionResetter.sh
+++ permissionResetter.sh
@@ -2,14 +2,17 @@
 
 #permission resetter - if user presses, s: show permissions that were changed, r: reset changed permission back to old one
 
+	if [[ $1 == "-r" ]] ; then
+		#make some variable this = diff $2 (file) and ls -lh?
+		printf "%20s %20s %20s\n" "Old" "Current" "File"
 
-	read var1 var2
-	reset=-r
-	show=-s
-
-	if [[ var1 = show ]] ; do
+		echo hi;
 	
-	elif [[ var1 == reset ]] ; do
+	elif [[ $1 == "-s" ]] ; then
+		printf "%20s %20s %20s\n" "Old" "Current" "File"
+	else
+		echo no;
+	fi
 
 
 	
